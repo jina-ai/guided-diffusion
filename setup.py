d70ddfe81b9d835202659b71321aea67460cbefe
Han Xiao <han.xiao@jina.ai>
2022-07-16 12:33:49 +0200

    fix: fp16

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="guided-diffusion",
-    py_modules=["guided_diffusion"],
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
 )
